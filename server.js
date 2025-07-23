d0efc9d2ef3394ba4620b4c56700cad1a10a2836
Deko911 <dmortizs2023@gmail.com>
2025-07-23 14:10:08 -0500

    Middleware para la información sobre las peticiones

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const app = express()
 const baseRouter = require('./src/routes/base.routes')
+const requestLogger = require('./src/middleware/requestLogger')
 
 app.use(express.static('./public'))
 app.use(express.json())
+
+app.use(requestLogger)
 
 app.use('/', baseRouter)
 
