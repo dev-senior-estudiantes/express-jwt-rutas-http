81f8f01ce99228124aaa09b3a14ea94b9b41e7ff
Geral866 <geralquintero2000@gmail.com>
2025-07-23 12:28:55 -0500

    jwt-rutas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registrar_usuario, iniciar_sesion, ruta_protegida } = require('../controllers/authcontroller');
+const {ruta_protegida} = require('../controllers/authcontroller');
 const verificar_token = require('../middleware/authMiddleware');
 
 router.get('/protected', verificar_token, ruta_protegida);
+
+
 
