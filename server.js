7a0dcd570dae72d79f0f6035fc119191ff51808c
OscarLLC <oscarlopez88991@gmail.com>
2025-07-24 19:50:16 -0500

    feat: creacion del server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+//Se realiza la importacion de las dependecias
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const authRoutes = require('authRoutes');
+const { send } = require('process');
+
+//configuracion inicial 
+dotenv.config();
+const app = express();
+
+//uso de Meddleware
+app.use(express.json()) //parsea el cuerpo del JSON y lo pasa a obj
+app.use(express.static(path.join(__dirname, 'public'))); // archivos estaticos publicos
+
+//esta es la ruta de autenticacion
+app.use('/api/auth', authRoutes)
+
+//esta es la ruta de autenticacion 
+app.get('/', (res, req) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+
+// puerto del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`servidor escuchando en http://localhost:${PORT}`)
+});
