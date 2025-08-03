a9e8cd6e8286f571e617b2a8e6c92e6de4e787bf
OscarLLC <oscarlopez88991@gmail.com>
2025-08-03 10:38:55 -0500

    feat: se modifico el regiter.js mejorando el flujo de trabajo

--- public/js/register.js
+++ public/js/register.js
@@ -1,35 +1,64 @@
-//con el selector de etiquetas accedemos al id del form
-//sera asincrona para que trabeje paralelamente
-document
-  .getElementById("registerForm")
-  .addEventListener("submit", async (e) => {
-    //evita que la pagina se recargue
+//accedemos al id del formulario
+    const form = document.getElementById('registerForm').addEventListener('submit', async (e)=>{
+    //prevenimos el recargue del la pagina cuando se envian los datos
     e.preventDefault();
 
-    //aqui obtenemos los datos del input y usamos el trim()
-    //para elimiar los espacios al inicio y fin
-    const usu = document.getElementById("user").value.trim();
-    const pass = document.getElementById("user").value.trim();
+    //accdemos al id del usuario
+    const usu = document.getElementById('username'.value.trim());
 
-    //usamos el try para capturar errores que pasen el feth
-    try {
-      //hacemos una peticion HTTP POST a la ruta /register
-      //usamos el await para esperar la respuesta que viene
-      //del servidor
-      const res = await fetch("/register", {
-        //aqui le indico el metodo en este caso sera el POST
-        method: "POST",
-        //indica el cuerpo de la peticion en formato JSON
-        headers: { "Content-Type": "application/json" },
-        //convertimos en cadena JSON el obj { usu, pass } la
-        //que enviare al servidor
-        body: JSON.stringify({ usu, pass }),
-      });
+     //accdemos al pass del usuario
+    const pass = document.getElementById('password'.value.trim());
 
-      //usamos el await donde el tiene que espar la respuesta
-      //del servidor
-      const data = await res.json();
-    } catch (err) {
-      console.log("error ");
+    //accdemos al parrafo donde se mostraran los mensajes de estado
+    const stateMessage = document.getElementById('statusMessage').value.trim();
+
+    //validamos si los campos estan vacios
+    if(!usu || !pass){
+        //aqui le digo que muestre un aviso
+        stateMessage.textContent = "Llenar todos loc campos";
+        //ese aviso viene de color rojo
+        stateMessage.style.color = "red";
+        //saco la funcion para que no siga
+        return;
     }
-  });
+
+
+    try{
+        //Se hace una solicitud POST al servidor usando fetch
+        const response = await fetch('/register', {
+            method: "POST",
+            headers: {
+                //aqui le digo que los datos que se estan enviado
+                // a la applicacion estan formato JSON
+                "Content-Type": "application/json()",
+            },
+
+            //tomo el obj y los pasamos a JSON para que lo entienda el servidor
+            body: JSON.stringify({ usu, pass})
+        });
+
+
+        //convierto la respuesta a un obj legible
+        const resul = await response.json();
+
+        //validamos si la respuesta fue exitosa
+        if(response.ok){
+            //muestra un mensaje de bienvenido
+            stateMessage.textContentn = "Bienvenido :))";
+            //con este color verde indicamos que todo esta bien
+            stateMessage.style.color = "green";
+            //limpio los campos del formulario
+            form.reset();
+        }else{
+            //muestra un mensaje si todo salio mal
+            stateMessage.textContentn = "Error validar credenciales :((";
+            //si hay error le ponemos color rojo
+            stateMessage.style.color = "red";
+        }
+    }catch(error){
+        //si hay un error se muestra un mensaje
+        stateMessage.textContent = "Ocurrio un problema intenta mas tarde";
+        stateMessage.style.color = "red";
+        stateMessage.error("Error al intentar registarse", error)
+    }
+});
